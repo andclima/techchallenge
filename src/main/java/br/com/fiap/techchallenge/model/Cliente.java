d1993b0f6ed0c766582aa8c72fe9e160bb7aa94f
RenatoGhirelli <79719528+RenatoGhirelli@users.noreply.github.com>
2025-08-05 22:26:00 -0300

    Update Cliente.java
    import correto

--- src/main/java/br/com/fiap/techchallenge/model/Cliente.java
+++ src/main/java/br/com/fiap/techchallenge/model/Cliente.java
@@ -1,10 +1,6 @@
 package br.com.fiap.techchallenge.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
