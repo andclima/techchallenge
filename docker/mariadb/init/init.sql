fb2d12c825a58d971ab9ee4a2c1184fba8570985
RenatoGhirelli <79719528+RenatoGhirelli@users.noreply.github.com>
2025-09-02 19:58:50 -0300

    Criação tabelas restaurante e intenscardapio

--- docker/mariadb/init/init.sql
+++ docker/mariadb/init/init.sql
@@ -20,7 +20,37 @@ create table cliente (
      numeroFidelidade varchar(255) null unique,
      constraint cliente_usuario_idUsuario_fk
          foreign key (idUsuario) references usuario (idUsuario)
+
 );
+
+create table restaurante (
+    idRestaurante int auto_increment primary key,
+    nome                    varchar(255) not null,
+    endereco                varchar(255) not null,
+    tipoCozinha             varchar(255) not null,
+    horarioFuncionamento    varchar(255) not null,
+    constraint fk_dono_restaurante
+        foreign key (idDono) references donorestaurante(idUsuario),
+    dataAtualizacao datetime(6)
+
+)
+
+create table itenscardarpio(
+    idItem int auto_increment primary key,
+    nome varchar(255) not null,
+    descricao varchar(255) null,
+    preco decimal(10,2) not null,
+    apenasLocal boolean not null default false,
+    foto varchar(255) null,
+    constraint fk_id_restaurante
+        foreign key (idRestaurante) references restaurante(idRestaurante),
+    dataAtualizacao datetime(6)
+)
+
+
+
+
+
 
 insert into usuario (nome, email, username, password, endereco, dataAtualizacao)
        values ('Administrador', 'admin@gmaillcom', 'admin',
@@ -29,3 +59,17 @@ insert into usuario (nome, email, username, password, endereco, dataAtualizacao)
 
 insert into donorestaurante (idUsuario, nomeDoRestaurante)
        values (1, 'Restaurante do Sr Admin');
+
+
+
+insert into restaurante (nome, endereco, tipoCozinha, horarioFuncionamento, idDono, dataAtualizacao)
+        values ('Para Lanches', 'esquina, 1444', 'Lanchonete', 'Seg-Qua:11h-22h; Sab-Dom: 11h-01h', 1, current_date);
+
+insert into itenscardapio (nome, descricao, peco, apenasLocal, foto, idRestaurante, dataAtualizacao)
+        values ('X-burguer', 'Pão, Carne e Queijo', 17.99, false, '', 1, current_date)
+
+
+
+
+
+
